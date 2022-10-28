d9d97ca6887e6e57facac6ff955e4375015e8bd6
mattritchey <115810694+mattritchey@users.noreply.github.com>
2022-10-28 09:33:31 -0400

    Update windtrial.py

--- windtrial.py
+++ windtrial.py
@@ -1,5 +1,3 @@
-
-
 import streamlit as st
 from streamlit_folium import st_folium
 import pandas as pd
@@ -13,6 +11,8 @@ import rasterio
 import numpy as np
 import branca.colormap as cm
 from matplotlib import colors as colors
+from joblib import Parallel, delayed
+import plotly.express as px
 import os
 import glob
 
@@ -21,11 +21,15 @@ try:
 except:
     pass
 
+st.set_page_config(layout="wide")
+col1, col2 = st.columns((2))
+
 address = st.sidebar.text_input("Address", "123 Main Street, Columbus, OH 43215")
 d = st.sidebar.date_input("Date",  pd.Timestamp(2022,9,28)).strftime('%Y%m%d')
 
 time = st.sidebar.selectbox('Time:',('12 AM','6 AM', '12 PM', '6 PM',))
 type_wind = st.sidebar.selectbox('Type:',('Gust','Wind'))
+entire_day = st.sidebar.selectbox('Graph Entire Day (Takes a Bit):',('No','Yes'))
 
 if time[-2:]=='PM' and int(time[:2].strip())<12:
    t= datetime.time(int(time[:2].strip())+12, 00).strftime('%H')+'00'
@@ -37,19 +41,20 @@ year,month,day=d[:4],d[4:6],d[6:8]
 url=f'https://mtarchive.geol.iastate.edu/{year}/{month}/{day}/grib2/ncep/RTMA/{d}{t}_{type_wind.upper()}.grib2'
 file=urllib.request.urlretrieve(url, f'{d}{t}{type_wind}.grib2')[0]
 
+
 geolocator = Nominatim(user_agent="GTA Lookup")
 geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
 
 location = geolocator.geocode(address)
 
 lat,lon=location.latitude,location.longitude
-map_data = pd.DataFrame({'lat': [lat], 'lon': [lon]})
+# map_data = pd.DataFrame({'lat': [lat], 'lon': [lon]})
+
 
 rds = rioxarray.open_rasterio(file)
 projected = rds.rio.reproject("EPSG:4326")
 wind_mph=projected.sel(x=lon, y=lat, method="nearest").values*2.23694
 
-st.write(f"{type_wind.title()} Speed: {wind_mph[0].round(2)} MPH")
 
 def mapvalue2color(value, cmap): 
 
@@ -78,8 +83,6 @@ clon = (left + right)/2
 vmin = np.floor(np.nanmin(img))
 vmax = np.ceil(np.nanmax(img))
 
-#colormap = cm.linear.RdBu_11.scale(vmin, vmax)
-
 colormap = cm.LinearColormap(colors=['blue','lightblue','red'], vmin=vmin,vmax=vmax)
 
 m = folium.Map(location=[lat, lon],  zoom_start=9,height=500)
@@ -101,7 +104,35 @@ folium.LayerControl().add_to(m)
 colormap.caption = 'Wind Speed: MPH'
 m.add_child(colormap)
 
-st_folium(m,height=500)
+with col1:
+    st.write(f"{type_wind.title()} Speed: {wind_mph[0].round(2)} MPH at {time} UTC")
+    st_folium(m,height=500)
+
+
+if entire_day=='Yes':
+    times=[f'0{str(i)}'[-2:] for i in range(0,24)]
+    urls=[ f'https://mtarchive.geol.iastate.edu/{year}/{month}/{day}/grib2/ncep/RTMA/{d}{t}00_{type_wind.upper()}.grib2' for t in times]
+    url=urls[0]
+    
+    def download_file_get_data(url):
+        file=urllib.request.urlretrieve(url, url[-23:])[0]
+        rds = rioxarray.open_rasterio(file)
+        wind_mph = rds.rio.reproject("EPSG:4326")[0,rows,columns].values*2.23694
+        time=url[-15:-11]
+        return [wind_mph,time]
+    
+    results=Parallel(n_jobs=-1)(delayed(download_file_get_data)(i) for i in urls)
+    
+    df_all=pd.DataFrame(results,columns=['MPH','Time'])
+    df_all['MPH']=df_all['MPH'].round(2)
+    df_all['Time']=pd.to_datetime(d+df_all['Time'],format = '%Y%m%d%H%M')
+    
+   
+    fig = px.line(df_all, x = "Time", y = "MPH")
+    with col2:
+        st.plotly_chart(fig)
+else:
+    pass
 
 st.markdown(""" <style>
 #MainMenu {visibility: hidden;}
