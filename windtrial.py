d5a8c88589da22cb7fb30058eafe641fca83aa11
mattritchey <115810694+mattritchey@users.noreply.github.com>
2022-11-16 08:54:33 -0500

    Update windtrial.py

--- windtrial.py
+++ windtrial.py
@@ -5,46 +5,67 @@ Created on Fri Oct 14 10:35:25 2022
 @author: mritchey
 """
 
-import streamlit as st
-from streamlit_folium import st_folium
-import pandas as pd
-import folium
-from geopy.geocoders import Nominatim
-from geopy.extra.rate_limiter import RateLimiter
 import datetime
-import urllib.request
-import rioxarray
-import rasterio
-import numpy as np
-import branca.colormap as cm
-from matplotlib import colors as colors
-from joblib import Parallel, delayed
-import plotly.express as px
-import os
 import glob
+import os
+import urllib.request
+import branca.colormap as cm
+import folium
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import rasterio
+import rioxarray
+import streamlit as st
+from geopy.extra.rate_limiter import RateLimiter
+from geopy.geocoders import Nominatim
+from joblib import Parallel, delayed
+from matplotlib import colors as colors
+from streamlit_folium import st_folium
+from threading import Thread
 
-def download_file_get_data(url):
-        file = urllib.request.urlretrieve(url, url[-23:])[0]
-        rds = rioxarray.open_rasterio(file)
-        wind_mph = rds.rio.reproject("EPSG:4326")[
-            0, rows, columns].values*2.23694
-        time = url[-15:-11]
-        return [wind_mph, time]
+def download_file_get_data(url,rows, columns):
+    file = urllib.request.urlretrieve(url, url[-23:])[0]
+    rds = rioxarray.open_rasterio(file)
+    wind_mph = rds.rio.reproject("EPSG:4326")[0, rows, columns].values*2.23694
+    time = url[-15:-11]
+    return [wind_mph, time]
+
+def threading(df_input,func_input):
+    starttime=time.time()
+    tasks_thread=df_input
+    results_thread=[]
+    def thread_func(value_input):
+        response=func_input(value_input)
+        results_thread.append(response)
+        return True
     
+    threads = []
+    for i in range(len(tasks_thread)):
+        process = Thread(target=thread_func, args=[tasks_thread[i]])
+        process.start()
+        threads.append(process)   
+        
+    for process in threads:
+        process.join()
+    print(f'Time: {str(round((time.time()-starttime)/60,5))} Minutes') 
+    return results_thread
+
 def mapvalue2color(value, cmap):
-    
     if np.isnan(value):
         return (1, 0, 0, 0)
     else:
         return colors.to_rgba(cmap(value), 0.7)
 
-@st.cache
+
+
 def geocode(address):
-    try: 
-        address2=address.replace(' ','+').replace(',','%2C')
-        df=pd.read_json(f'https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address={address2}&benchmark=2020&format=json')
-        results=df.iloc[:1,0][0][0]['coordinates']
-        lat, lon=results['y'],results['x']
+    try:
+        address2 = address.replace(' ', '+').replace(',', '%2C')
+        df = pd.read_json(
+            f'https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address={address2}&benchmark=2020&format=json')
+        results = df.iloc[:1, 0][0][0]['coordinates']
+        lat, lon = results['y'], results['x']
     except:
         geolocator = Nominatim(user_agent="GTA Lookup")
         geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
@@ -53,28 +74,30 @@ def geocode(address):
     return lat, lon
 
 @st.cache
-def get_grib_data(url, d,t):
+def get_grib_data(url, d, t):
     file = urllib.request.urlretrieve(url, f'{d}{t}{type_wind}.grib2')[0]
     return file
 
 @st.cache
-def graph_entire_day(d):
+def graph_entire_day(d,rows, columns):
     year, month, day = d[:4], d[4:6], d[6:8]
     times = [f'0{str(i)}'[-2:] for i in range(0, 24)]
-    urls = [f'https://mtarchive.geol.iastate.edu/{year}/{month}/{day}/grib2/ncep/RTMA/{d}{t}00_{type_wind.upper()}.grib2' for t in times]
+    urls = [
+        f'https://mtarchive.geol.iastate.edu/{year}/{month}/{day}/grib2/ncep/RTMA/{d}{t}00_{type_wind.upper()}.grib2' for t in times]
     
     results = Parallel(n_jobs=4)(
-        delayed(download_file_get_data)(i) for i in urls)
+        delayed(download_file_get_data)(i,rows, columns) for i in urls)
 
     df_all = pd.DataFrame(results, columns=['MPH', 'Time'])
     df_all['MPH'] = df_all['MPH'].round(2)
     df_all['Time'] = pd.to_datetime(d+df_all['Time'], format='%Y%m%d%H%M')
     return df_all
 
+
 @st.cache
 def convert_df(df):
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df.to_csv(index=0).encode('utf-8')
+
 
 try:
     for i in glob.glob('*.grib2'):
@@ -96,16 +119,16 @@ entire_day = st.sidebar.radio(
     'Graph Entire Day (Takes a Bit):', ('No', 'Yes'))
 
 if time[-2:] == 'PM' and int(time[:2].strip()) < 12:
-   t = datetime.time(int(time[:2].strip())+12, 00).strftime('%H')+'00'
+    t = datetime.time(int(time[:2].strip())+12, 00).strftime('%H')+'00'
 elif time[-2:] == 'AM' and int(time[:2].strip()) == 12:
-   t = '0000'
+    t = '0000'
 else:
-   t = datetime.time(int(time[:2].strip()), 00).strftime('%H')+'00'
+    t = datetime.time(int(time[:2].strip()), 00).strftime('%H')+'00'
 
 year, month, day = d[:4], d[4:6], d[6:8]
 
 url = f'https://mtarchive.geol.iastate.edu/{year}/{month}/{day}/grib2/ncep/RTMA/{d}{t}_{type_wind.upper()}.grib2'
-file=get_grib_data(url, d, t)
+file = get_grib_data(url, d, t)
 
 lat, lon = geocode(address)
 
@@ -163,14 +186,14 @@ with col1:
 
 
 if entire_day == 'Yes':
-    df_all= graph_entire_day(d)
+    df_all = graph_entire_day(d,rows, columns)
     fig = px.line(df_all, x="Time", y="MPH")
     with col2:
         st.title('Analysis')
         st.plotly_chart(fig)
-        
+
         csv = convert_df(df_all)
-        
+
         st.download_button(
             label="Download data as CSV",
             data=csv,
